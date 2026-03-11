972861783dc51a811b48216048d12c197954c715
Mithunraj V R <vrmithunraj@gmail.com>
2026-03-11 05:19:05 +0000

    update commit

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -2,7 +2,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox"
-], function (Controller,MessageBox)  {
+], function (Controller, MessageBox) {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
@@ -18,8 +18,6 @@ sap.ui.define([
             this.hiddleAllpanels();
             var oPanel = this.byId("_IDGenHBox2");
             oPanel.setVisible(true);
-           
-
             var oButton = oEvent.getSource();
 
 
@@ -41,54 +39,60 @@ sap.ui.define([
             var oPanel = this.byId("Panel3");
             oPanel.setVisible(true);
         },
-        EnterDetails(){
+        EnterDetails() {
             this.hiddleAllpanels();
-            var oPanel=this.byId("Panel4")
+            var oPanel = this.byId("Panel4")
             oPanel.setVisible(true);
         },
         hiddleAllpanels() {
             this.byId("_IDGenHBox2").setVisible(false)
             this.byId("Panel2").setVisible(false);
             this.byId("Panel3").setVisible(false);
-
+            // this.byId("Panel4").setVisible(true);
         },
         onSubmit: function () {
             var participantsName = this.byId("participantsName").getValue();
             var email = this.byId("email").getValue();
             var phone = this.byId("phone").getValue();
-
-
             var oModel = this.getView().getModel();
+
+
+            if (participantsName=="" ||email==""|| phone =="" ) {
+                sap.m.MessageBox.error("Please fill all fields");
+                return
+
+            }
+            var regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!regx.test(email)) {
+
+                sap.m.MessageBox.error("Invalid Email");
+
+                return; // stop execution
+            }
+
+
+            var participantsName = this.byId("participantsName").getValue();
 
             var oEventContext = oModel.bindList("/participants").create({
                 participantsName: participantsName,
                 email: email,
                 phone: phone,
                 peopleRegistration_ID: this.selectedEventID
-            });
 
+            });
             console.log(oEventContext);
-            
-            oEventContext.created().then(()=>{
+            oEventContext.created().then(() => {
                 MessageBox.success("Details are to be added ");
-             participantsName = this.byId("participantsName").setValue(null);
-            email = this.byId("email").setValue(null);
-            phone = this.byId("phone").setValue(null);
+                participantsName = this.byId("participantsName").setValue(null);
+                email = this.byId("email").setValue(null);
+                phone = this.byId("phone").setValue(null);
 
-               if (!regx.test(email)) {
-
-        sap.m.MessageBox.error("Invalid Email");
-
-        return; // stop execution
-               }
-
-            }).catch((err)=>{
-                 console.error(err);
+            }).catch((err) => {
+                console.error(err);
                 MessageBox.error("Error details are not entered");
-                console.error("Error details not to be entered :" +err);
-                
-            });
+                console.error("Error details not to be entered :" + err);
 
+            });
         }
     });
 });
